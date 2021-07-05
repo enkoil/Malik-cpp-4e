3342636f08e8cdd7d2edfaa83dc53f722288e974
Nikole Varhegyi <varhegyin@gmail.com>
2021-07-05 18:23:34 -0400

    Chapter 2, Programming Exercise 3 solution

---
+++ programming-exercises/Chapter-02/problem-03.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1 = 125, num2 = 28, num3 = -25;
+    int average = (num1 + num2 + num3) / 3;
+    
+    cout << "The average of " << num1 << ", " << num2 << ", and "
+         << num3 << " is " << average << endl;
+    
+    return 0;
+}
