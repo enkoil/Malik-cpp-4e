539e933b5cb03c77f8741b6d17e26b38a567037b
Nikole Varhegyi <nvarhegyi3@gatech.edu>
2021-07-05 17:50:10 -0400

    Chapter 2, Programming Exercise 1 solution

---
+++ programming-exercises/Chapter-02/problem-01.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "**********************************" << endl;
+    cout << "*    Programming Assignment 1    *" << endl;
+    cout << "*     Computer Programming I     *" << endl;
+    cout << "*         Author: enkoil         *" << endl;
+    cout << "*   Due Date: Tuesday, Jul. 05   *" << endl;
+    cout << "**********************************" << endl;
+
+    return 0;
+}
